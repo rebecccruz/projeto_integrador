039c843d767fc374b7a4f2765e8d6c9b17ae2e87
LucasPinheiro <lucas.rpinheiro@mercadolivre.com>
2022-08-08 15:14:31 -0300

    feat: modeling batch stock

--- src/main/java/br/com/dh/meli/projeto_integrador/model/BatchStock.java
+++ src/main/java/br/com/dh/meli/projeto_integrador/model/BatchStock.java
@@ -2,19 +2,39 @@ package br.com.dh.meli.projeto_integrador.model;
 
 import lombok.*;
 
+import javax.persistence.*;
 import javax.validation.constraints.*;
 import java.time.*;
 
 @Data
+@Entity
 @Builder
 public class BatchStock {
-    public int batchNumber;
+    @Id
+    @Column(name = "id")
+    public Integer batchNumber;
+
+    @Column(name = "product_sku", nullable = false)
     public String productId;
+
+    @Column(name = "current_temperature", nullable = false)
     public Float currentTemperature;
+
+    @Column(name = "minimum_temperature", nullable = false)
     public Float minimumTemperature;
-    public int initialQuantity;
-    public int currentQuantity;
+
+    @Column(name = "initial_quantity", nullable = false)
+    public Integer initialQuantity;
+
+    @Column(name = "current_quantity", nullable = false)
+    public Integer currentQuantity;
+
+    @Column(name = "manufacturing_date", nullable = false)
     public LocalDate manufacturingDate;
+
+    @Column(name = "manufacturing_time", nullable = false)
     public LocalDateTime manufacturingTime;
+
+    @Column(name = "due_date", nullable = false)
     public LocalDate dueDate;
 }
