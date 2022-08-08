ec74e35704ac9e753ff429d7cff137346f88b8e1
LucasPinheiro <lucas.rpinheiro@mercadolivre.com>
2022-08-08 15:31:30 -0300

    feat: modeling section entity

--- src/main/java/br/com/dh/meli/projeto_integrador/model/Section.java
+++ src/main/java/br/com/dh/meli/projeto_integrador/model/Section.java
@@ -1,19 +1,29 @@
 package br.com.dh.meli.projeto_integrador.model;
 
 import br.com.dh.meli.projeto_integrador.enums.Category;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.List;
 
 @Data
-@AllArgsConstructor
+@Entity
 @NoArgsConstructor
 public class Section {
-    private String sectionCode;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "category_id", nullable = false)
     private Category category;
-    private Float temperatureRange;
+
+    @Column(nullable = false)
     private Integer capacity;
-    private List<Product> product;
+
+    @OneToMany(mappedBy = "section")
+    @JsonIgnoreProperties("section")
+    private List<BatchStock> batchStocks;
 }
