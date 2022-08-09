c76833a2a0ba820d3ff027bc091319db7db57eb3
aborgessouzameli <alexandre.bsouza@mercadolivre.com>
2022-08-09 08:26:38 -0300

    chore: removed empty line

--- src/main/java/br/com/dh/meli/projeto_integrador/model/geolocalization/CountryModel.java
+++ src/main/java/br/com/dh/meli/projeto_integrador/model/geolocalization/CountryModel.java
@@ -7,7 +7,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-
 import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
