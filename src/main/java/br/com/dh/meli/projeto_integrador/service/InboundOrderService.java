b2a943573393dbe2f13b10e493dfb5f8dbdd0450
Isaias <isaias.finger@mercadolivre.com>
2022-08-09 17:45:01 -0300

    feat: WIP Implements InboundOrderService

--- src/main/java/br/com/dh/meli/projeto_integrador/service/InboundOrderService.java
+++ src/main/java/br/com/dh/meli/projeto_integrador/service/InboundOrderService.java
@@ -1,24 +1,73 @@
 package br.com.dh.meli.projeto_integrador.service;
 
 import br.com.dh.meli.projeto_integrador.dto.InboundOrderDTO;
-import br.com.dh.meli.projeto_integrador.model.BatchStock;
-import br.com.dh.meli.projeto_integrador.model.InboundOrder;
+import br.com.dh.meli.projeto_integrador.exception.BadRequestException;
+import br.com.dh.meli.projeto_integrador.mapper.IInboundOrderMapper;
+import br.com.dh.meli.projeto_integrador.model.*;
 import br.com.dh.meli.projeto_integrador.repository.IInboundOrderRepository;
+import br.com.dh.meli.projeto_integrador.repository.ISectionRepository;
+import br.com.dh.meli.projeto_integrador.repository.IWarehouseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.*;
 
 @Service
 public class InboundOrderService implements IInboundOrderService {
 
     @Autowired
     private IInboundOrderRepository repo;
+    @Autowired
+    private IWarehouseRepository warehouseRepo;
+    @Autowired
+    private ISectionRepository sectionRepo;
+
     @Override
-    public Void saveInboundOrder(BatchStock batchStock) {
-        return null;
+    public InboundOrderDTO createInboundOrder(InboundOrderDTO dto) {
+        Optional<Warehouse> warehouse = warehouseRepo.findWarehouseByCode(dto.getWarehouseCode());
+
+        // E que o armazém é válido
+        if (warehouse.isEmpty()) {
+            throw new BadRequestException("invalid wareHouseCode");
+        }
+
+        // E que o representante pertence ao armazém
+        Optional<Representant> representant = warehouse.get().getRepresentants()
+                .stream().filter(r -> r.getId().equals(dto.getRepresentantId())).findFirst();
+        if (representant.isEmpty()) {
+            throw new BadRequestException("invalid representId");
+        }
+
+        // E que o setor é válido
+        Optional<Section> section = warehouse.get().getSections()
+                .stream().filter(s -> s.getCode().equalsIgnoreCase(dto.getSectionCode())).findFirst();
+        if (section.isEmpty()) {
+            throw new BadRequestException("invalid sectionCode");
+        }
+
+        // TODO: E que o setor corresponde ao tipo de produto
+
+        // TODO: E que o setor tenha espaço disponível
+        int maxCapacity = section.get().getCapacity();
+        int currentCapacity = section.get().getBatchStocks().size();
+        int availableCapacity = maxCapacity - currentCapacity;
+        int neededCapacity = dto.getBatchStock().size();
+        boolean haveCapacity = availableCapacity >= neededCapacity;
+        if (haveCapacity) {
+            InboundOrder inboundOrder = IInboundOrderMapper.MAPPER.mappingInboundOrderDTOToInboundOrder(dto);
+            // TODO: Save in database
+            //repo.save(inboundOrder);
+        }
+
+        return dto;
     }
 
     @Override
-    public InboundOrderDTO createInboundOrder(InboundOrder inboundOrder) {
-        return null;
+    public InboundOrderDTO updateInboundOrder(InboundOrderDTO dto) {
+        // TODO: Update inboundOrder from dto
+        return dto;
+    }
+
+    private void saveBatchStock(BatchStock batchStock) {
+
     }
 }
