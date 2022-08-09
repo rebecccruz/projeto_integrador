5fed36f53f5df5fa853ff3ad5e2b427197cdb00a
aborgessouzameli <alexandre.bsouza@mercadolivre.com>
2022-08-09 08:57:45 -0300

    chore: refatored after code review

--- src/main/java/br/com/dh/meli/projeto_integrador/dto/geolocalization/UpdateCountryDTO.java
+++ src/main/java/br/com/dh/meli/projeto_integrador/dto/geolocalization/UpdateCountryDTO.java
@@ -10,10 +10,10 @@ import javax.validation.constraints.Size;
 @Data
 @NoArgsConstructor
 public class UpdateCountryDTO {
-    @NotNull(message = "Título é obrigatório")
+    @NotNull(message = "Nome do país é obrigatório")
     @JsonProperty("country_name")
     private String name;
-    @NotNull(message = "Sigla é obrigatório")
+    @NotNull(message = "Sigla é obrigatório. Deve conter mínimo e máximo 2 caracteres.")
     @Size(min = 2, max = 2)
     @JsonProperty("country_initials")
     private String initials;
