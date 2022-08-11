6f902759f3bd7f1179993799b266e3227534ff34
Isaias <isaias.finger@mercadolivre.com>
2022-08-11 15:49:06 -0300

    feat: BatchStock service

---
+++ src/main/java/br/com/dh/meli/projeto_integrador/service/BatchStockService.java
@@ -0,0 +1,32 @@
+package br.com.dh.meli.projeto_integrador.service;
+
+import br.com.dh.meli.projeto_integrador.dto.BatchStockDTO;
+import br.com.dh.meli.projeto_integrador.mapper.IBatchStockMapper;
+import br.com.dh.meli.projeto_integrador.model.BatchStock;
+import br.com.dh.meli.projeto_integrador.model.InboundOrder;
+import br.com.dh.meli.projeto_integrador.repository.IBatchStockRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class BatchStockService implements IBatchStockService {
+
+    @Autowired
+    private IBatchStockRepository repo;
+
+    public List<BatchStock> batchStockMapper(List<BatchStockDTO> batches, InboundOrder inboundOrder) {
+        return batches.stream().map(dto -> {
+            BatchStock batch = IBatchStockMapper.MAPPER.mappingBatchStockDTOToBatchStock(dto);
+            batch.setInboundOrder(inboundOrder);
+            batch.setSection(inboundOrder.getSection());
+            return batch;
+        }).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<BatchStock> saveAll(List<BatchStock> batches) {
+        return repo.saveAll(batches);
+    }
+}
---
+++ src/main/java/br/com/dh/meli/projeto_integrador/service/IBatchStockService.java
@@ -0,0 +1,11 @@
+package br.com.dh.meli.projeto_integrador.service;
+
+import br.com.dh.meli.projeto_integrador.dto.BatchStockDTO;
+import br.com.dh.meli.projeto_integrador.model.BatchStock;
+import br.com.dh.meli.projeto_integrador.model.InboundOrder;
+import java.util.*;
+
+public interface IBatchStockService {
+    List<BatchStock> batchStockMapper(List<BatchStockDTO> batches, InboundOrder inboundOrder);
+    List<BatchStock> saveAll(List<BatchStock> batches);
+}
