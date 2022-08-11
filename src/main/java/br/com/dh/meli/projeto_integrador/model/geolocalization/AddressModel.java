3db63bb653f45113efbd994d195c2e6915b6e95e
aborgessouzameli <alexandre.bsouza@mercadolivre.com>
2022-08-11 16:50:54 -0300

    hotfix: changed reference columns address/warehouse

--- src/main/java/br/com/dh/meli/projeto_integrador/model/geolocalization/AddressModel.java
+++ src/main/java/br/com/dh/meli/projeto_integrador/model/geolocalization/AddressModel.java
@@ -35,5 +35,6 @@ public class AddressModel {
     private ZipCodeModel zip;
 
     @OneToOne(fetch = FetchType.LAZY)
-    private Warehouse warehouse;
+    @PrimaryKeyJoinColumn
+    private AddressModel address;
 }
