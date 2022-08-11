2d4dcb451261c6ccae4d0a06672b88ab1e1285ae
Isaias <isaias.finger@mercadolivre.com>
2022-08-11 15:49:28 -0300

    feat: Warehouse service

---
+++ src/main/java/br/com/dh/meli/projeto_integrador/service/WarehouseService.java
@@ -0,0 +1,38 @@
+package br.com.dh.meli.projeto_integrador.service;
+
+import br.com.dh.meli.projeto_integrador.exception.BadRequestException;
+import br.com.dh.meli.projeto_integrador.exception.NotFoundException;
+import br.com.dh.meli.projeto_integrador.exception.PreconditionFailedException;
+import br.com.dh.meli.projeto_integrador.model.Representant;
+import br.com.dh.meli.projeto_integrador.model.Warehouse;
+import br.com.dh.meli.projeto_integrador.repository.IWarehouseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class WarehouseService implements IWarehouseService {
+
+    @Autowired
+    private IWarehouseRepository repo;
+
+    @Override
+    public Warehouse findWarehouseByCode(String code) {
+        Optional<Warehouse> warehouse = repo.findWarehouseByCode(code);
+        if(warehouse.isEmpty()) {
+            throw new PreconditionFailedException("warehouse code not found");
+        }
+        return warehouse.get();
+    }
+
+    @Override
+    public Representant findRepresentantFromWarehouse(Warehouse warehouse, Long id) {
+        Optional<Representant> representant = warehouse.getRepresentants()
+                .stream().filter(r -> r.getId().equals(id)).findFirst();
+        if (representant.isEmpty()) {
+            throw new BadRequestException("invalid representId");
+        }
+        return representant.get();
+    }
+}
