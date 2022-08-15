aa28cdd1eebe5c7721c21e06e768ca1067501d98
Isaias <isaias.finger@mercadolivre.com>
2022-08-15 20:44:32 -0300

    feat: ItemServiceTests

--- src/test/java/br/com/dh/meli/projeto_integrador/util/ItemUtil.java
+++ src/test/java/br/com/dh/meli/projeto_integrador/util/ItemUtil.java
@@ -2,6 +2,7 @@ package br.com.dh.meli.projeto_integrador.util;
 
 import br.com.dh.meli.projeto_integrador.dto.ItemDTO;
 import br.com.dh.meli.projeto_integrador.model.Item;
+import br.com.dh.meli.projeto_integrador.model.ShoppingCart;
 
 public class ItemUtil {
 
@@ -10,11 +11,18 @@ public class ItemUtil {
      * @author Evelyn Cristini
      * @return returns a static item
      */
-    public static Item emptyItem () {
+    public static Item emptyItem() {
         Item item = new Item();
         item.setId(1L);
         item.setQuantity(1);
+        return item;
+    }
 
+    public static Item completeItem() {
+        Item item = emptyItem();
+        item.setAdvertisement(AdvertisementUtil.advertisementGenerator());
+        item.setBatchStock(BatchStocksTestUtil.listOfBatchStock().stream().findFirst().get());
+        item.setShoppingCart(ShoppingCartUtil.shoppingCartGenerator());
         return item;
     }
 
@@ -23,12 +31,11 @@ public class ItemUtil {
      * @author Evelyn Cristini
      * @return returns a dto
      */
-    public static ItemDTO emptyItemDTO () {
+    public static ItemDTO emptyItemDTO() {
         ItemDTO dto = new ItemDTO();
         dto.setQuantity(1);
         dto.setAdvertisementId(1L);
         dto.setBatchNumber(1);
-
         return dto;
     }
 }
