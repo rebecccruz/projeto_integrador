cc379776050f7b24fa40642efffee1e58ceddd89
Isaias <isaias.finger@mercadolivre.com>
2022-08-16 11:13:44 -0300

    hotfix: InboundOrderControllerTest

--- src/test/java/br/com/dh/meli/projeto_integrador/controller/InboundOrderControllerTest.java
+++ src/test/java/br/com/dh/meli/projeto_integrador/controller/InboundOrderControllerTest.java
@@ -1,17 +1,22 @@
 package br.com.dh.meli.projeto_integrador.controller;
 
 import br.com.dh.meli.projeto_integrador.dto.InboundOrderDTO;
+import br.com.dh.meli.projeto_integrador.model.InboundOrder;
+import br.com.dh.meli.projeto_integrador.service.IInboundOrderService;
 import br.com.dh.meli.projeto_integrador.util.InboundOrderUtil;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,13 +26,28 @@ class InboundOrderControllerTest {
     @InjectMocks
     private InboundOrderController controller;
 
+    @Mock
+    private IInboundOrderService service;
+
+    @BeforeEach
+    public void setup() {
+        BDDMockito.when(service.createInboundOrder(ArgumentMatchers.any(InboundOrderDTO.class)))
+                .thenReturn(InboundOrderUtil.inboundOrderGenerator());
+
+        BDDMockito.when(service.updateInboundOrder(ArgumentMatchers.any(InboundOrderDTO.class)))
+                .thenReturn(InboundOrderUtil.inboundOrderGenerator());
+
+        BDDMockito.when(service.convertToDto(ArgumentMatchers.any(InboundOrder.class)))
+                .thenReturn(InboundOrderUtil.inboundOrderDTOGenerator());
+
+    }
+
     @DisplayName("Create new batchStocks when payload is valid")
     @Test
     void createBatchStock() {
         InboundOrderDTO payload = InboundOrderUtil.inboundOrderDTOGenerator();
 
         ResponseEntity<InboundOrderDTO> response = controller.createInboundOrder(payload);
-
         assertThat(response.getStatusCode())
                 .isEqualTo(HttpStatus.CREATED);
         assertThat(response.getBody().getBatchStock().size())
